f3720dbf9311ece877e38078bb9767084558f4e9
Colin Jaffe <balloonasaurus@gmail.com>
2020-02-03 20:36:06 -0500

    Adds tests and adjusts string formats.

--- main.test.js
+++ main.test.js
@@ -7,7 +7,7 @@ const {
 } = require('./main.js')
 
 describe('isItLong', () => {
-  it('returns undefined if given a short string', () => {
+  it('returns undefined if given a string under 20 characters', () => {
     expect(isItLong('hi')).toBe(undefined)
   })
   
@@ -21,63 +21,78 @@ describe('isItLong', () => {
 })
 
 describe('isItMedium', () => {
-  it('returns undefined if given a short string', () => {
+  it('returns undefined if given a string under 10 characters', () => {
     expect(isItMedium('hi')).toBe(undefined)
   })
   
-  it('returns undefined if given a long string', () => {
+  it('returns undefined if given a string over 20 characters', () => {
     expect(isItMedium(`I'm 21 chars long!!!!`)).toBe(undefined)
   })
   
-  it(`returns 'That\'s a regular sized string!' if the string is exactly 10 characters`, () => {
+  it(`returns "That's a regular sized string!" if the string is exactly 10 characters`, () => {
     expect(isItMedium(`I'm Medium`)).toBe(`That's a regular sized string!`)
   })
+
+  it(`returns "That's a regular sized string!" if the string is between 10 and 20 characters`, () => {
+    expect(isItMedium(`I'm in the middle!`)).toBe(`That's a regular sized string!`)
+    expect(isItMedium(`And so am I!`)).toBe(`That's a regular sized string!`)
+  })
   
-  it(`returns 'That\'s a regular sized string!' if given a string of exactly 20 characters`, () => {
-    expect(isItMedium(`I'm 20 chars long...`)).toBe('That\'s a regular sized string!')
+  it(`returns "That's a regular sized string!" if given a string of exactly 20 characters`, () => {
+    expect(isItMedium(`I'm 20 chars long...`)).toBe(`That's a regular sized string!`)
   })
 })
 
 describe('isItShort', () => {
-  it('returns undefined if given a long string', () => {
+  it('returns undefined if given a string over 10 characters long.', () => {
     expect(isItShort(`I'm 20 chars long...`)).toBe(undefined)
   })
+
+  it(`returns undefined if given a string of exactly 10 characters`, () => {
+    expect(isItShort(`I'm Medium`)).toBe(undefined)
+  })
   
-  it('returns `That\'s a small string!` if given a string of fewer than 10 characters', () => {
+  it(`returns "That's a small string!" if given a string of fewer than 10 characters`, () => {
     expect(isItShort(`I'm Short`)).toBe(`That's a small string!`)
   })
 })
 
 describe('howLongIsMyString', () => {
-  it(`should return 'That\'s a long string!' given a string of more than 20 characters.`, () => {
+  it(`should return "That's a long string!" given a string of more than 20 characters.`, () => {
     expect(howLongIsMyString(`I'm 21 chars long!!!!`)).toBe(`That's a long string!`);
   })
 
   it(`should return 
-  'That\'s a regular sized string!' given a string of exactly 20 characters.`, () => {
+  "That's a regular sized string!" given a string of exactly 20 characters.`, () => {
     expect(howLongIsMyString(`I'm 20 chars long...`)).toBe(`That's a regular sized string!`);
   })
 
   it(`should return 
-  'That\'s a regular sized string!' given a string of exactly 10 characters.`, () => {
+  "That's a regular sized string!" given a string of between 10 and 20 characters.`, () => {
+    expect(howLongIsMyString(`Not long or short!`)).toBe(`That's a regular sized string!`);
+    expect(howLongIsMyString(`About yea long...`)).toBe(`That's a regular sized string!`);
+  })
+
+  it(`should return 
+  "That's a regular sized string!" given a string of exactly 10 characters.`, () => {
     expect(howLongIsMyString(`I'm Medium`)).toBe(`That's a regular sized string!`);
   })
 
-  it(`should return 'That\'s a small string!' if given a string of fewer than 10 characters`, () => {
+  it(`should return "That's a small string!" if given a string of fewer than 10 characters`, () => {
     expect(howLongIsMyString(`Shorty!!!`)).toBe(`That's a small string!`);
   })
 })
 
 describe('instructorHeight', () => {
-  it('should return 62 if given the string `Colin`', () => {
+  it(`should return 62 if given the string 'Colin'`, () => {
     expect(instructorHeight('Colin')).toBe(62)
   })
 
-  it('should return 67 if given the string `Mesuara`', () => {
+  it(`should return 67 if given the string 'Mesuara'`, () => {
     expect(instructorHeight('Mesuara')).toBe(67)
   })
   
-  it(`should return 'I don\'t know that instructor!' given any other argument.`, () => {
+  it(`should return "I don't know that instructor!" given any other argument.`, () => {
     expect(instructorHeight('Anthony')).toBe(`I don't know that instructor!`);
   })
 })
