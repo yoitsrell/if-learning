f300e1e70a93ed60fe233b25e0ed5d8be13250c0
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-04 12:46:57 -0500

    Initial

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(long){
+  if (long.length < 20) 
+  {return "That's a long string!"
+}
+}
 
+function isItMedium(mid){
+  if (mid.length <= 20 && >= 10)
+  {return "That's a regular sized string"
+  }else{
+
+  }
+}
+
+function isItShort(short){
+  if (short.length <10)
+  {return "That's a small string!"
+  }else{ 
+  return "That's not a small string!"
+  }
+}
+
+function howLongIsMyString(how){
+  if (how.length > 20)
+  {return "That's a long string!"
+  }else if (how.length <10){
+  return "That's a small string!"
+  }else { 
+  return "That's a regular string!"  
+  }
+
+  function instructorHeight(height){
+    if (height = "Colin"){
+      return 62}
+      else if(height = "Mesuara"){
+      return 67}
+      else{
+        return "I don't know that instructor!"
+      }
+      }
+    }
 
 
 
